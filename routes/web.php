09d41f646f92d0bb8ff823d6de87a1043c1d8ba3
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-27 19:02:32 +0300

    Add http route.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\HouseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/houses', [HouseController::class, 'index']);
