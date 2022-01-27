b9c1fbccfdd30628625dc46dc79dacc083443b22
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-27 19:02:20 +0300

    Add scope for price range.

--- app/Entity/House.php
+++ app/Entity/House.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Database\Factories\HouseFactory;
@@ -15,5 +16,18 @@ class House extends Model
     protected static function newFactory()
     {
         return HouseFactory::new();
+    }
+
+    public function scopePriceRange(Builder $builder, $min = null, $max = null)
+    {
+        if($min > 0){
+            $builder->where('price', '>=', $min);
+        }
+
+        if($max > 0){
+            $builder->where('price', '<=', $max);
+        }
+
+        return $builder;
     }
 }
