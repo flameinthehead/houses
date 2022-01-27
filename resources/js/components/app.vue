5a9f72b84fb2a5eff76a2f2fe4e9fcba662d6dd0
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-27 23:58:47 +0300

    Bootstrap styles.

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,26 +1,86 @@
 <template>
-    <div>
+    <div class="container-sm">
+        <h3 class="text-center">Houses</h3>
         <form ref="form" v-on:submit="search">
-            <input name="name" v-model="name" placeholder="Name" />
-            <input name="price_from" v-model="price_from" placeholder="Price from" />
-            <input name="price_to" v-model="price_to" placeholder="Price to" /><br />
-            <input name="bedrooms" v-model="bedrooms" placeholder="Bedrooms" />
-            <input name="bathrooms" v-model="bathrooms" placeholder="Bathrooms" />
-            <input name="storeys" v-model="storeys" placeholder="Storeys" />
-            <input name="garages" v-model="garages" placeholder="Garages" />
-            <button type="submit">Search</button>
-        </form>
-        <p v-if="error_message">{{ error_message }}</p>
-        <p v-if="nothing_found">Nothing found!</p>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input id="name" name="name" class="form-control" v-bind:class="{ 'is-invalid': errMess && errMess.name }" />
+                <span v-if="errMess && errMess.name" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.name[0] }}</strong>
+                </span>
+            </div>
+            <div class="mb-3">
+                <label for="price_from" class="form-label">Price from</label>
+                <input id="price_from" name="price_from" class="form-control" v-bind:class="{ 'is-invalid': errMess && (errMess.price_from || errMess.price_range) }" />
+                <span v-if="errMess && errMess.price_from" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.price_from[0] }}</strong>
+                </span>
+            </div>
 
-        <table v-if="houses.length">
+            <div class="mb-3">
+                <label for="price_to" class="form-label">Price to</label>
+                <input id="price_to" name="price_to" class="form-control" v-bind:class="{ 'is-invalid': errMess && (errMess.price_to || errMess.price_range) }" />
+                <span v-if="errMess && errMess.price_to" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.price_to[0] }}</strong>
+                </span>
+                <span v-if="errMess && errMess.price_range" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.price_range[0] }}</strong>
+                </span>
+            </div>
+
+            <div class="mb-3">
+                <label for="bedrooms" class="form-label">Bedrooms</label>
+                <input id="bedrooms" name="bedrooms" class="form-control" v-bind:class="{ 'is-invalid': errMess && errMess.bedrooms }" />
+                <span v-if="errMess && errMess.bedrooms" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.bedrooms[0] }}</strong>
+                </span>
+            </div>
+
+            <div class="mb-3">
+                <label for="bathrooms" class="form-label">Bathrooms</label>
+                <input id="bathrooms" name="bathrooms" class="form-control" v-bind:class="{ 'is-invalid': errMess && errMess.bathrooms }" />
+                <span v-if="errMess && errMess.bathrooms" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.bathrooms[0] }}</strong>
+                </span>
+            </div>
+
+            <div class="mb-3">
+                <label for="storeys" class="form-label">Storeys</label>
+                <input id="storeys" name="storeys" class="form-control" v-bind:class="{ 'is-invalid': errMess && errMess.storeys }" />
+                <span v-if="errMess && errMess.storeys" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.storeys[0] }}</strong>
+                </span>
+            </div>
+
+            <div class="mb-3">
+                <label for="garages" class="form-label">Garages</label>
+                <input id="garages" name="garages" class="form-control" v-bind:class="{ 'is-invalid': errMess && errMess.garages }" />
+                <span v-if="errMess && errMess.garages" class="invalid-feedback" role="alert">
+                    <strong>{{ errMess.garages[0] }}</strong>
+                </span>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Search</button>
+            <input class="btn btn-secondary" v-on:click="reset" type="reset" value="Reset">
+        </form>
+
+        <div v-if="nothing_found" class="alert alert-primary mt-3" role="alert">
+            Nothing found!
+        </div>
+
+        <div v-if="errMess" class="alert alert-danger mt-3" role="alert">
+            Search error
+        </div>
+
+        <hr v-if="houses.length" />
+        <table v-if="houses.length" class="table">
             <thead>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Bedrooms</th>
-                <th>Bathrooms</th>
-                <th>Storeys</th>
-                <th>Garages</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Bedrooms</th>
+                <th scope="col">Bathrooms</th>
+                <th scope="col">Storeys</th>
+                <th scope="col">Garages</th>
             </thead>
             <tr v-for="house in houses">
                 <td>{{ house.name }}</td>
@@ -40,18 +100,22 @@ export default {
          this.search();
     },
     methods: {
-        search(e) {
+        search(e, reset = false) {
             if (typeof e != 'undefined'){
                 e.preventDefault();
             }
 
-            const formData = new FormData(this.$refs['form']); // reference to form element
-            const data = {}; // need to convert it before using not with XMLHttpRequest
-            for (let [key, val] of formData.entries()) {
-                Object.assign(data, { [key]: val })
+            const formData = new FormData(this.$refs['form']);
+            const data = {};
+            if (!reset){
+                for (let [key, val] of formData.entries()) {
+                    Object.assign(data, { [key]: val })
+                }
             }
 
             var output = this;
+            output.nothing_found = false;
+            output.errMess = null;
             axios
                 .get("/houses", {params: data})
                 .then(function(response) {
@@ -64,28 +128,26 @@ export default {
                     }
                 })
                 .catch(function(error) {
-                    var error_messages = [];
+                    output.houses = [];
                     if (error.response && error.response.data) {
                         var data = error.response.data;
-                        console.log(data);
-                        output.error_message = error_messages;
+                        output.errMess = data.message;
                     }
                 })
+        },
+
+        reset(e) {
+            this.$refs.form.reset();
+            this.search(e, true);
+            this.$refs.form.reset();
         }
     },
     data() {
         return {
             success: false,
-            error_message: null,
+            errMess: null,
             nothing_found: false,
             houses: [],
-            name: null,
-            price_from: null,
-            price_to: null,
-            bedrooms: null,
-            bathrooms: null,
-            storeys: null,
-            garages: null,
         };
     },
 };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div id="app">
         <main class="py-3">
-            <h3>Houses</h3>
             <router-view></router-view>
         </main>
     </div>
