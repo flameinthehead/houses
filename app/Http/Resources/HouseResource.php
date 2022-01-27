c63ad8bd1979ce569ebfd67aef63f06c18d417a2
Oleg Shmakov <flame.in.the.head@gmail.com>
2022-01-27 17:30:17 +0300

    Add house resource.

---
+++ app/Http/Resources/HouseResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class HouseResource extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'bedrooms' => $this->bedrooms,
+            'bathrooms' => $this->bathrooms,
+            'storeys' => $this->storeys,
+            'garages' => $this->garages,
+        ];
+    }
+}
